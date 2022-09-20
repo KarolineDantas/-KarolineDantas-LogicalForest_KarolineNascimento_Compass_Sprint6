d3eadeb9a7947dec3ce1f442d58631ba5c7b7d9c
Karoline Nascimento <107884724+KarolineDantas@users.noreply.github.com>
2022-09-20 11:37:57 -0300

    rota usuarios funcionando perfeitamente

--- cypress/integration/usuarios.spec.js
+++ cypress/integration/usuarios.spec.js
@@ -10,38 +10,42 @@ const URL_LOGIN = '/login'
 const URL_PRODUTOS = '/produtos'
 const URL_CARRINHOS = '/carrinhos'
 
-it('Cadastro de usuário com sucesso', () => {
-    Factory.gerarUsuario
-    Serverest.cadastrarUsuarioComSucesso().then( resposta => {
-        cy.contractValidation(resposta, 'post-usuarios', 201)
-        ValidaServerest.validarCadastroDeProdutoComSucesso(resposta)
-    })
-})
 
-it('Tentativa de cadastro com email já utilizado', () => {
-    Serverest.cadastrarUsuarioSemSucesso().then( resposta => {
-        cy.contractValidation(resposta, 'post-usuarios', 400)
-        ValidaServerest.validarCadastroUsuarioSemSucesso(resposta)
-    })
-})
+describe('Testes para a rota usuários', () => {
 
-it('Tentativa de cadastro com email inválido', () => {
-    Serverest.cadastroUsuarioEmailInvalido().then( resposta => {
-        cy.contractValidation(resposta, 'post-usuarios', 400)
-        ValidaServerest.validarCadastroUsuarioEmailInvalido(resposta)
+    it('Tentativa de cadastro com email já utilizado', () => {
+        Serverest.cadastrarUsuarioSemSucesso().then( resposta => {
+            cy.contractValidation(resposta, 'post-usuarios', 400)
+            ValidaServerest.validarCadastroUsuarioSemSucesso(resposta)
+        })
     })
-})
-
-it('Tentativa de cadastro com senha inválida', () => {
-    Serverest.cadastroUsuarioSenhaInvalida().then( resposta => {
-        cy.contractValidation(resposta, 'post-usuarios', 400)
-        ValidaServerest.validarCadastroUsuarioSenhaInvalida(resposta)
+    
+    it('Tentativa de cadastro com email inválido', () => {
+        Serverest.cadastroUsuarioEmailInvalido().then( resposta => {
+            cy.contractValidation(resposta, 'post-usuarios', 400)
+            ValidaServerest.validarCadastroUsuarioEmailInvalido(resposta)
+        })
     })
-})
+    
+    it('Tentativa de cadastro com senha inválida', () => {
+        Serverest.cadastroUsuarioSenhaInvalida().then( resposta => {
+            cy.contractValidation(resposta, 'post-usuarios', 400)
+            ValidaServerest.validarCadastroUsuarioSenhaInvalida(resposta)
+        })
+    })
+    
+    it('Tentativa de cadastro com nome inválido', () => {
+        Serverest.cadastroUsuarioNomeInvalido().then( resposta => {
+            cy.contractValidation(resposta, 'post-usuarios', 400)
+            ValidaServerest.validarCadastroUsuarioNomeInvalido(resposta)
+        })
+    })
 
-it('Tentativa de cadastro com nome inválido', () => {
-    Serverest.cadastroUsuarioNomeInvalido().then( resposta => {
-        cy.contractValidation(resposta, 'post-usuarios', 400)
-        ValidaServerest.validarCadastroUsuarioNomeInvalido(resposta)
+    it('Cadastro de usuário com sucesso', () => {
+        Factory.gerarUsuario
+        Serverest.cadastrarUsuarioComSucesso().then( resposta => {
+            cy.contractValidation(resposta, 'post-usuarios', 201)
+            ValidaServerest.validarCadastroDeProdutoComSucesso(resposta)
+        })
     })
 })
